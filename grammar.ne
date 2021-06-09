76d7f9c386595799e88e560b915b26a5ec99b55f
Lucas Lois <lucasloisp@gmail.com>
2021-06-09 19:29:57 -0300

    parses the atomic types

--- grammar.ne
+++ grammar.ne
@@ -2,42 +2,22 @@
 const moo = require('moo');
 const lexer = require('./lex');
 const functions = require('./functions');
+const ty = require('./types');
 
 %}
 @lexer lexer
-I ->
-    %If C %Then I %Else I {% ([_, cond, _2, then, _3, otherwise]) => cond ? then : otherwise %}
-  | C {% ([fst]) => fst %}
-C ->
-    E  {% ([fst]) => fst %}
-  | E %Equal E {% ([fst, _, snd]) => fst == snd %}
-  | E %Diff E {% ([fst, _, snd]) => fst != snd %}
-  | E %GTE E {% ([fst, _, snd]) => fst >= snd %}
-  | E %GT E {% ([fst, _, snd]) => fst > snd %}
-  | E %LT E {% ([fst, _, snd]) => fst < snd %}
-  | E %LTE E {% ([fst, _, snd]) => fst <= snd %}
-  | E %And E {% ([fst, _, snd]) => fst && snd %}
-  | E %Or E {% ([fst, _, snd]) => fst || snd %}
-
-E ->
-    T {% ([fst]) => fst %}
-  | E %Add T {% ([fst, _, snd]) => fst + snd %}
-  | E %Sub T {% ([fst, _, snd]) => fst - snd %}
-T ->
-    N {% ([fst]) => fst %}
-  | T %Mult N {% ([fst, _, snd]) => fst * snd %}
-  | T %Div N {% ([fst, _, snd]) => fst / snd %}
-  | T %ZDiv N {% ([fst, _, snd]) => Math.floor(fst / snd) %}
-  | T %Mod N {% ([fst, _, snd]) => fst % snd %}
-N ->
-    %Number {% ([fst]) => +fst %}
-  | %True {% ([_]) => true %}
-  | %False {% ([_]) => false %}
-  | %Sub N {% ([fst, snd]) => -snd %}
-  | %ParOpen I %ParClose {% ([_, fst, _2]) => fst %}
-  | %Not N {% ([_, snd]) => !snd %}
-  | %Identifier %ParOpen ARG %ParClose {% ([f, _, n, _2]) => functions.get(f.value)(...n) %}
-  | %Identifier %ParOpen %ParClose {% ([f, _, _2]) => functions.get(f.value)() %}
-ARG ->
-    I {% ([fst]) => [fst] %}
-  | ARG %Comma I {% ([fst, _, snd]) => fst.concat(snd) %}
+ATOMIC ->
+    %Undefined {% () => ty.undefinedType() %}
+  | %Boolean {% () => ty.booleanType() %}
+  | %Number {% () => ty.numberType() %}
+  | %String {% () => ty.stringType() %}
+  | %Function {% () => ty.functionType() %}
+  | %Symbol {% () => ty.symbolType() %}
+  | %Object {% () => ty.objectType() %}
+  | %BigInt {% () => ty.bigintType() %}
+  | %Void {% () => ty.voidType() %}
+  | %Int {% () => ty.intType() %}
+  | %Double {% () => ty.doubleType() %}
+  | %Char {% () => ty.charType() %}
+  | %Byte {% () => ty.byteType() %}
+  | %Any {% () => ty.anyType() %}
--- grammar.test.js
+++ grammar.test.js
@@ -1,147 +1,37 @@
 const nearley = require("nearley");
 const grammar = require("./grammar.js");
+const ty = require("./types");
 
 let parser;
 beforeEach(() => {
-  parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 });
 
 function expectToUnambiguouslyEvaluateTo(string, value) {
-  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-  let { results: parseResult } = parser.feed(string);
-  expect(parseResult.length).toBe(1);
-  expect(parseResult).toContain(value);
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    let {results: parseResult} = parser.feed(string);
+    expect(parseResult.length).toBe(1);
+    expect(parseResult).toContainEqual(value);
 }
 
 describe("the language's grammar", () => {
-  describe("the primitives in the language", () => {
-    test("numbers are a part of the grammar", () => {
-      expectToUnambiguouslyEvaluateTo("0", 0);
-      expectToUnambiguouslyEvaluateTo("188498210", 188498210);
+    describe("the atomic types", () => {
+        test("parses the atomic types", () => {
+            expectToUnambiguouslyEvaluateTo("undefined", ty.undefinedType());
+            expectToUnambiguouslyEvaluateTo("boolean", ty.booleanType());
+            expectToUnambiguouslyEvaluateTo("number", ty.numberType());
+            expectToUnambiguouslyEvaluateTo("string", ty.stringType());
+            expectToUnambiguouslyEvaluateTo("function", ty.functionType());
+            expectToUnambiguouslyEvaluateTo("symbol", ty.symbolType());
+            expectToUnambiguouslyEvaluateTo("object", ty.objectType());
+            expectToUnambiguouslyEvaluateTo("bigint", ty.bigintType());
+            expectToUnambiguouslyEvaluateTo("void", ty.voidType());
+            expectToUnambiguouslyEvaluateTo("int", ty.intType());
+            expectToUnambiguouslyEvaluateTo("double", ty.doubleType());
+            expectToUnambiguouslyEvaluateTo("char", ty.charType());
+            expectToUnambiguouslyEvaluateTo("byte", ty.byteType());
+            expectToUnambiguouslyEvaluateTo("any", ty.anyType());
+        });
+        // expectToUnambiguouslyEvaluateTo("bigint", ty.bigintType());
     });
-    test("booleans are a part of the grammar", () => {
-      expectToUnambiguouslyEvaluateTo("true", true);
-      expectToUnambiguouslyEvaluateTo("false", false);
-    });
-  });
-  describe("boolean operators", () => {
-    describe("not operator", () => {
-      test("flips boolean primitives", () => {
-        expectToUnambiguouslyEvaluateTo("!true", false);
-        expectToUnambiguouslyEvaluateTo("!false", true);
-      });
-      test("acts on the truthiness of numbers", () => {
-        expectToUnambiguouslyEvaluateTo("!0", true);
-        expectToUnambiguouslyEvaluateTo("!1", false);
-        expectToUnambiguouslyEvaluateTo("!124124", false);
-      });
-    });
-  });
-  describe("arithmetic operators", () => {
-    describe("negation operator", () => {
-      test("gives negative numbers", () => {
-        expectToUnambiguouslyEvaluateTo("-1", -1);
-        expectToUnambiguouslyEvaluateTo("-144", -144);
-      });
-      test("is idempotent on zero", () => {
-        expectToUnambiguouslyEvaluateTo("-0", 0);
-      });
-      test("is its own inverse", () => {
-        expectToUnambiguouslyEvaluateTo("--1", 1);
-        expectToUnambiguouslyEvaluateTo("--124142", 124142);
-        expectToUnambiguouslyEvaluateTo("--0", 0);
-      });
-    });
-    describe("addition operator", () => {
-      test("adds positive numeric primitives", () => {
-        expectToUnambiguouslyEvaluateTo("1+1", 2);
-        expectToUnambiguouslyEvaluateTo("0+1", 1);
-        expectToUnambiguouslyEvaluateTo("1+0", 1);
-        expectToUnambiguouslyEvaluateTo("1124+6321", 1124 + 6321);
-      });
-      test("adds negated numbers", () => {
-        expectToUnambiguouslyEvaluateTo("-1+1", 0);
-        expectToUnambiguouslyEvaluateTo("1+(-1)", 0);
-        expectToUnambiguouslyEvaluateTo("14+(-1)", 13);
-      });
-      test("adds booleans as numeric bits", () => {
-        expectToUnambiguouslyEvaluateTo("1+false", 1);
-        expectToUnambiguouslyEvaluateTo("1+true", 2);
-        expectToUnambiguouslyEvaluateTo("false+1", 1);
-        expectToUnambiguouslyEvaluateTo("true+1", 2);
-        expectToUnambiguouslyEvaluateTo("true+true", 2);
-        expectToUnambiguouslyEvaluateTo("false+true", 1);
-        expectToUnambiguouslyEvaluateTo("false+false", 0);
-        expectToUnambiguouslyEvaluateTo("true+false", 1);
-      });
-    });
-    describe("multiplication operator", () => {
-      test("multiplies positive numeric primitives", () => {
-        expectToUnambiguouslyEvaluateTo("1*1", 1);
-        expectToUnambiguouslyEvaluateTo("0*1", 0);
-        expectToUnambiguouslyEvaluateTo("1*0", 0);
-        expectToUnambiguouslyEvaluateTo("1423*1", 1423);
-        expectToUnambiguouslyEvaluateTo("1*1423", 1423);
-        expectToUnambiguouslyEvaluateTo("1124*6321", 1124 * 6321);
-      });
-      test("multiplies negated numbers", () => {
-        expectToUnambiguouslyEvaluateTo("-1*1", -1);
-        expectToUnambiguouslyEvaluateTo("1*(-1)", -1);
-        expectToUnambiguouslyEvaluateTo("14*(-1)", -14);
-      });
-      test("multiplies booleans as numeric bits", () => {
-        expectToUnambiguouslyEvaluateTo("1*false", 0);
-        expectToUnambiguouslyEvaluateTo("1*true", 1);
-        expectToUnambiguouslyEvaluateTo("false*1", 0);
-        expectToUnambiguouslyEvaluateTo("true*1", 1);
-        expectToUnambiguouslyEvaluateTo("true*true", 1);
-        expectToUnambiguouslyEvaluateTo("false*true", 0);
-        expectToUnambiguouslyEvaluateTo("false*false", 0);
-        expectToUnambiguouslyEvaluateTo("true*false", 0);
-      });
-    });
-  });
-  describe("division operator", () => {
-    test("divides positive numeric primitives", () => {
-      expectToUnambiguouslyEvaluateTo("1/1", 1);
-      expectToUnambiguouslyEvaluateTo("0/1", 0);
-      expectToUnambiguouslyEvaluateTo("1/0", Infinity);
-      expectToUnambiguouslyEvaluateTo("1423/1", 1423);
-      expectToUnambiguouslyEvaluateTo("1/1423", 1 / 1423);
-      expectToUnambiguouslyEvaluateTo("1124/6321", 1124 / 6321);
-    });
-    test("divides negated numbers", () => {
-      expectToUnambiguouslyEvaluateTo("-1/1", -1);
-      expectToUnambiguouslyEvaluateTo("1/(-1)", -1);
-      expectToUnambiguouslyEvaluateTo("14/(-1)", -14);
-    });
-    test("divides booleans as numeric bits", () => {
-      expectToUnambiguouslyEvaluateTo("1/false", Infinity);
-      expectToUnambiguouslyEvaluateTo("1/true", 1);
-      expectToUnambiguouslyEvaluateTo("false/1", 0);
-      expectToUnambiguouslyEvaluateTo("true/1", 1);
-      expectToUnambiguouslyEvaluateTo("true/true", 1);
-      expectToUnambiguouslyEvaluateTo("false/true", 0);
-      expectToUnambiguouslyEvaluateTo("true/false", Infinity);
-    });
-  });
-  describe("precedence rules", () => {
-    test("multiplication precedes addition", () => {
-      expectToUnambiguouslyEvaluateTo("2*2+3*4", 2 * 2 + 3 * 4);
-      expectToUnambiguouslyEvaluateTo("1+3*4", 1 + 3 * 4);
-      expectToUnambiguouslyEvaluateTo(
-        "1231*1412+1823*1241",
-        1231 * 1412 + 1823 * 1241
-      );
-    });
-  });
-  describe("parentheses", () => {
-    test("are superfluous on terminals", () => {
-      expectToUnambiguouslyEvaluateTo("(0)", 0);
-      expectToUnambiguouslyEvaluateTo("(1)", 1);
-      expectToUnambiguouslyEvaluateTo("(124124)", 124124);
-      expectToUnambiguouslyEvaluateTo("(true)", true);
-      expectToUnambiguouslyEvaluateTo("(false)", false);
-    });
-  });
 });
