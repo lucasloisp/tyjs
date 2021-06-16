cf06a8bab59983c8b239312c5a9587de1054882e
Lucas Lois <lucasloisp@gmail.com>
2021-06-16 18:48:05 -0300

    changes type tests to match object

--- types.test.js
+++ types.test.js
@@ -3,52 +3,52 @@ const ty = require("./types.js");
 describe("the types in TyJS", () => {
   describe("the atomic types", () => {
     test("the undefined type", () => {
-      expect(ty.undefinedType()).toEqual({ type: "undefined" });
+      expect(ty.undefinedType()).toMatchObject({ type: "undefined" });
     });
     test("the boolean type", () => {
-      expect(ty.booleanType()).toEqual({ type: "boolean" });
+      expect(ty.booleanType()).toMatchObject({ type: "boolean" });
     });
     test("the number type", () => {
-      expect(ty.numberType()).toEqual({ type: "number" });
+      expect(ty.numberType()).toMatchObject({ type: "number" });
     });
     test("the string type", () => {
-      expect(ty.stringType()).toEqual({ type: "string" });
+      expect(ty.stringType()).toMatchObject({ type: "string" });
     });
     test("the function type", () => {
-      expect(ty.functionType()).toEqual({ type: "function" });
+      expect(ty.functionType()).toMatchObject({ type: "function" });
     });
     test("the symbol type", () => {
-      expect(ty.symbolType()).toEqual({ type: "symbol" });
+      expect(ty.symbolType()).toMatchObject({ type: "symbol" });
     });
     test("the object type", () => {
-      expect(ty.objectType()).toEqual({ type: "object" });
+      expect(ty.objectType()).toMatchObject({ type: "object" });
     });
     test("the bigint type", () => {
-      expect(ty.bigintType()).toEqual({ type: "bigint" });
+      expect(ty.bigintType()).toMatchObject({ type: "bigint" });
     });
     test("the void type", () => {
-      expect(ty.voidType()).toEqual({ type: "void" });
+      expect(ty.voidType()).toMatchObject({ type: "void" });
     });
     test("the int type", () => {
-      expect(ty.intType()).toEqual({ type: "int" });
+      expect(ty.intType()).toMatchObject({ type: "int" });
     });
     test("the double type", () => {
-      expect(ty.doubleType()).toEqual({ type: "double" });
+      expect(ty.doubleType()).toMatchObject({ type: "double" });
     });
     test("the char type", () => {
-      expect(ty.charType()).toEqual({ type: "char" });
+      expect(ty.charType()).toMatchObject({ type: "char" });
     });
     test("the byte type", () => {
-      expect(ty.byteType()).toEqual({ type: "byte" });
+      expect(ty.byteType()).toMatchObject({ type: "byte" });
     });
     test("the any type", () => {
-      expect(ty.anyType()).toEqual({ type: "any" });
+      expect(ty.anyType()).toMatchObject({ type: "any" });
     });
   });
   describe("the type operators", () => {
     describe("the not operator", () => {
       test("it has a reference to another type", () => {
-        expect(ty.not(ty.undefinedType())).toEqual({
+        expect(ty.not(ty.undefinedType())).toMatchObject({
           type: "not",
           left: ty.undefinedType(),
         });
