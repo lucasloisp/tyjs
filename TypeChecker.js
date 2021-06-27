08457faa98b4408d6c52551d49a7c0b2c38c28ed
Lucas Lois <lucasloisp@gmail.com>
2021-06-27 17:58:21 -0300

    adds the static classChecker method

--- TypeChecker.js
+++ TypeChecker.js
@@ -1,33 +1,22 @@
 const nearley = require("nearley");
 const grammar = require("./grammar.js");
 
-function genericSequenceChecker(obj, generics) {
-  return generics.length === 1 && Array.from(obj).every((v) => generics[0](v));
-}
-function mapChecker(obj, generics) {
-  return (
-    generics.length === 2 &&
-    Array.from(obj.entries()).every(
-      ([k, v]) => generics[0](k) && generics[1](v)
-    )
-  );
-}
-
 class Type {
+  static globalCheckers = new Map();
+
   constructor(typeDescription, checkFunctions = []) {
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     const { results: parseResult } = parser.feed(typeDescription);
     this.typeTree = parseResult[0];
     this.classCheckers = new Map();
-    this.classCheckers.set("Array", genericSequenceChecker);
-    this.classCheckers.set("Set", genericSequenceChecker);
-    this.classCheckers.set("Map", mapChecker);
   }
 
   getClassCheckerFor(cls) {
     const checker = this.classCheckers.get(cls);
-    if (!checker) throw new Error(`${cls} has no checking for generics`);
-    return checker;
+    const globalChecker = Type.globalCheckers.get(cls);
+    if (!checker && !globalChecker)
+      throw new Error(`${cls} has no checking for generics`);
+    return checker || globalChecker;
   }
 
   checks(value) {
@@ -39,6 +28,27 @@ class Type {
   classChecker(cls, checker) {
     this.classCheckers.set(cls.name, checker);
   }
+
+  static classChecker(cls, checker) {
+    Type.globalCheckers.set(cls.name, checker);
+  }
 }
+
+function genericSequenceChecker(obj, generics) {
+  return generics.length === 1 && Array.from(obj).every((v) => generics[0](v));
+}
+
+function mapChecker(obj, generics) {
+  return (
+    generics.length === 2 &&
+    Array.from(obj.entries()).every(
+      ([k, v]) => generics[0](k) && generics[1](v)
+    )
+  );
+}
+
+Type.classChecker(Array, genericSequenceChecker);
+Type.classChecker(Set, genericSequenceChecker);
+Type.classChecker(Map, mapChecker);
 
 module.exports = Type;
